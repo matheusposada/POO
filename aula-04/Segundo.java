d7959809097eab535bd5591e6df2fcdc39cb50c7
Matheus Posada <matheusposada@gmail.com>
2026-02-19 20:17:59 -0300

    Relembrando Switch case java 25

---
+++ aula-04/Segundo.java
@@ -0,0 +1,16 @@
+void main() {
+    int i = 1;
+
+    String s;
+
+    switch (i) {
+        case 1:
+            s = "Um";
+            break;
+        default:
+            s = "Outro";
+            break;
+    }
+
+    IO.println(s);
+}
